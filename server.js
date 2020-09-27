1119ff2ff351daf6af40cf72e7b58ab42373931f
rlmineing_dead <3838shah@gmail.com>
2020-09-27 15:43:49 +0000

    added a decent server.js

--- server.js
+++ server.js
@@ -4,16 +4,7 @@ var path = require('path');
 
 // viewed at http://localhost:8080
 app.get('*', function(req, res) {
-    
-    if (req.url) {
-        
-    }
-    try {
-        res.sendFile(path.join(__dirname + req.url));
-    } catch (error) {
-        res.sendFile("404.html");
-    }
-    
+    res.send("<h1>yeet</h1>");
 });
 
 
